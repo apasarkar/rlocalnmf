3a9e610ee592dca63d1a636b705de7fe7f45b8aa
apasarkar <amolpasarkar@gmail.com>
2024-06-03 17:12:34 -0400

    Updates dependencies to include torch dependencies

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
 setuptools.setup(
     name="rlocalnmf",
     version="0.0.4",
     description="New implementation of localnmf with advanced background models and initialization options",
     packages=setuptools.find_packages(),
-    install_requires=["numpy", "scipy","cvxpy","Cython", "networkx","scikit-learn", "torch", "matplotlib", "opencv-python", "scikit-image"],
+    install_requires=["torch", "torchvision", "torchaudio", "numpy", "scipy", "cvxpy", "Cython",
+                      "networkx",
+                      "scikit-learn", "matplotlib",
+                      "opencv-python", "scikit-image"],
     classifiers=(
         "Programming Language :: Python :: 3",
     ),
