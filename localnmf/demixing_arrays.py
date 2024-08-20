df0519c4ad03608c246a5e2b48d3432b2c47e304
apasarkar <amolpasarkar@gmail.com>
2024-08-21 06:36:35 +0800

    Includes updated gpu accelerated computations for fluctuating background update

--- localnmf/demixing_arrays.py
+++ localnmf/demixing_arrays.py
@@ -161,7 +161,7 @@ class ACVideo(FactorizedVideo):
 
         return product.squeeze()
 
-class FluctuatingBGVideo(FactorizedVideo):
+class FluctuatingBkgdVideo(FactorizedVideo):
     """
     Factorized video for the spatial and temporal extracted sources from the data
 
@@ -169,29 +169,56 @@ class FluctuatingBGVideo(FactorizedVideo):
     "c" is the matrix of temporal profiles
     """
 
-    def __init__(self, fov_shape: tuple[int, int, int], order: str, w: scipy.sparse.coo_matrix,
-                 b: np.ndarray, u: scipy.sparse.coo_matrix, r: np.ndarray, s: np.ndarray, v: np.ndarray):
+    def __init__(self, fov_shape: tuple[int, int], order: str, u: scipy.sparse.coo_matrix,
+                 r: np.ndarray, q: np.ndarray, v: np.ndarray, device: str='cpu'):
         """
+        The background movie can be factorized as as the matrix product (u)(r)(q)(v),
+        where u, r, and v are the standard matrices from the pmd decomposition,
         Args:
             fov_shape (tuple): (fov_dim1, fov_dim2)
             order (str): Order to reshape arrays from 1D to 2D
-            w (scipy.sparse.coo_matrix): shape (pixels, pixels)
-            b (np.ndarray): shape (pixels, 1)
             u (scipy.sparse.coo_matrix): shape (pixels, rank1)
-            r (scipy.sparse.coo_matrix): shape (rank1, rank2)
-            s (np.ndarray): shape (rank2,)
+            r (np.ndarray): shape (rank1, rank2)
+            q (np.ndarray): shape (rank 2, rank 2)
             v (np.ndarray): shape (rank2, frames)
+            device (str): which device the computations take place on (cuda or cpu)
         """
+        self._device = device
         t = v.shape[1]
         self._shape = (t,) + fov_shape
-        self.w = w.tocsr()
-        self.b = b
-        self.u = u
-        self.v = v
-        self.r = r
-        self.s = s
+        self._u_orig = u
+        self._v_orig = v
+        self._r_orig = r
+        self._q_orig = q
+
+        self._u = torch.sparse_coo_tensor(np.array(u.nonzero()), u.data, u.shape).coalesce().float().to(device)
+        self._v = torch.from_numpy(v).float().to(device)
+        self._r = torch.from_numpy(r).float().to(device)
+        self._q = torch.from_numpy(q).float().to(device)
         self.pixel_mat = np.arange(np.prod(self.shape[1:])).reshape([self.shape[1], self.shape[2]], order=order)
+        self.pixel_mat = torch.from_numpy(self.pixel_mat).long().to(self.device)
         self._order = order
+
+    @property
+    def device(self) -> str:
+        return self._device
+
+    @property
+    def u(self) -> scipy.sparse.coo_matrix:
+        return self._u_orig
+
+    @property
+    def r(self) -> np.ndarray:
+        return self._r_orig
+
+    @property
+    def q(self) -> np.ndarray:
+        return self._s_orig
+
+    @property
+    def v(self) -> np.ndarray:
+        return self._v_orig
+
 
     @property
     def dtype(self) -> str:
@@ -221,7 +248,7 @@ class FluctuatingBGVideo(FactorizedVideo):
         """
         return len(self.shape)
 
-    def _getitem(
+    def __getitem__(
             self,
             item: Union[int, list, np.ndarray, Tuple[Union[int, np.ndarray, slice, range]]]
     ):
@@ -241,7 +268,7 @@ class FluctuatingBGVideo(FactorizedVideo):
         # Step 2: Do some basic error handling for frame_indexer before using it to slice
 
         if isinstance(frame_indexer, np.ndarray):
-            frame_indexer = frame_indexer
+            frame_indexer = torch.from_numpy(frame_indexer).long().to(self.device)
 
         if isinstance(frame_indexer, list):
             pass
@@ -283,60 +310,39 @@ class FluctuatingBGVideo(FactorizedVideo):
             )
 
         # Step 3: Now slice the data with frame_indexer (careful: if the ndims has shrunk, add a dim)
-        v_crop = self.v[:, frame_indexer]
-        if v_crop.ndim < self.v.ndim:
-            v_crop = v_crop[:, None]
+        v_crop = self._v[:, frame_indexer]
+        if v_crop.ndim < self._v.ndim:
+            v_crop = v_crop.unsqueeze(1)
 
         # Step 4: Deal with remaining indices after lazy computing the frame(s)
         if isinstance(item, tuple):
             pixel_space_crop = self.pixel_mat[item[1:]]
-            w_indices = pixel_space_crop.reshape((-1), order=self.order)
-            
-            w_crop = self.w[w_indices, :]
+            u_indices = pixel_space_crop.flatten()
+            u_crop = torch.index_select(self._u, 0, u_indices)
             implied_fov = pixel_space_crop.shape
         else:
-            w_crop = self.w
+            u_crop = self._u
             implied_fov = self.shape[1], self.shape[2]
 
         # Temporal term is guaranteed to have nonzero "T" dimension below
         if np.prod(implied_fov) <= v_crop.shape[1]:
-            wu_crop = w_crop.dot(self.u)
-            wur_crop = wu_crop.dot(self.r)
-            wurs_crop = wur_crop * self.s[None, :]
-            wursv_crop = wurs_crop.dot(v_crop)
-            b_term = w_crop.dot(self.b)
-            product = wursv_crop - b_term
+            product = torch.sparse.mm(u_crop, self._r)
+            product = torch.matmul(product, self._q)
+            product  = torch.matmul(product, v_crop)
+
         else:
-            vs_crop = self.s[:, None] * v_crop
-            rvs_crop = self.r.dot(vs_crop)
-            urvs_crop = self.u.dot(rvs_crop)
-            wurvs_crop = w_crop.dot(urvs_crop)
-            b_term = w_crop.dot(self.b)
-            product = wurvs_crop - b_term
+            product = torch.matmul(self._q, v_crop)
+            product = torch.matmul(self._r, product)
+            product = torch.matmul(self._u, product)
 
-        product = product.reshape(implied_fov + (-1,), order=self.order)
-
-        num_dims = product.ndim
-        # This will put the last dimension first, followed by the remaining dimensions in order
-        new_order = (num_dims - 1,) + tuple(range(num_dims - 1))
-        product = np.transpose(product, axes=new_order)
+        product = product.reshape(implied_fov + (-1,))
+        product = product.permute(-1, *range(product.ndim - 1))
+        product = product.cpu().numpy().astype(self.dtype)
         return product.squeeze()
-
-    def __getitem__(
-            self,
-            item: Union[int, list, np.ndarray, Tuple[Union[int, np.ndarray, slice, range]]]
-    ):
-
-        output = self._getitem(item)
-        return np.array(output)
-
 
 class ResidualVideo(FactorizedVideo):
     """
     Factorized video for the spatial and temporal extracted sources from the data
-
-    "a" is the matrix of spatial profiles
-    "c" is the matrix of temporal profiles
     """
 
     def __init__(self, pmd_video: FactorizedVideo, fluctuating_bg_video: FactorizedVideo,
